cf7c030883c5dd0e1a45569afa163874c0223fdf
joy <ciftekharulislam@gmail.com>
2021-07-20 16:25:51 +0600

    edit dom.js

--- dom.js
+++ dom.js
@@ -77,3 +77,113 @@ document.querySelector('li:last-child(even)').style.background = '#f4f4f4';
 // console.log (val);
 // console.log (allVal);
 console.log (accessOneVal);
+
+// document.getElementsByClassName
+
+const items = document.getElementsByClassName('collection-item');
+console.log(items);
+console.log(items[0]);
+
+items[0].style.color = 'red';
+items[3].textContent = "Hello";
+
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+console.log(listItems);
+
+// document.getElementsByTagName
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello';
+
+// convert HTML collections into array
+lis = Array.from(lis);
+lis.reverse();
+
+lis.forEach(function(li, index) {
+    console.log(li.className);
+    li.textContent = `${index}: hello`;
+});
+
+console.log(lis);
+
+// document.querySelectorAll
+
+// create the elements
+
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item'
+
+// Add id
+li.id = 'new-item';
+
+// Add attribute
+
+li.setAttribute('title', 'New Item');
+
+// create text node 
+
+console.log(li);
+
+// removing and replacing elements
+
+// create Element
+
+const newHeading = document.createElement('h2');
+
+newHeading.id = 'task-title';
+
+newHeading.appendChild(document.createTextNode('Task List'));
+
+// Get the old heading
+
+const oldHeading = document.getElementById('task-title');
+
+// parent
+
+const cardAction = document.querySelector('.card-action');
+
+// replace
+
+cardAction.replaceChild(newHeading, oldHeading);
+
+// REMOVE ELEMENT
+
+const lis = document.querySelectorAll('li');
+const ulLis = document.querySelector('ul');
+
+// Remove list item
+
+lis [0].remove();
+
+// Remove child element 
+
+ulLis.removeChild(lis[3]);
+
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+
+// Attributes
+
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+
+
+console.log(newHeading);
